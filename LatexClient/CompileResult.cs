ac2a80348def1aefd2fb8391453cedea8acfdeca
Matt Jarvis <mattj23@gmail.com>
2021-07-15 10:48:40 -0400

    extracted interfaces and restricted base classes

--- LatexClient/CompileResult.cs
+++ LatexClient/CompileResult.cs
@@ -7,7 +7,35 @@ using Flurl.Http;
 
 namespace LatexClient
 {
-    public class CompileResult
+    public interface ICompileResult
+    {
+        /// <summary>
+        /// Gets whether or not the compilation completed successfully. If the compilation was not successful
+        /// there will be no product.
+        /// </summary>
+        bool IsSuccessful { get; }
+
+        /// <summary>
+        /// Asynchronously retrieve the compilation log, which can be used to debug errors in the case that the
+        /// compilation failed.
+        /// </summary>
+        /// <returns>A string containing the entire compiler log</returns>
+        Task<string> GetLog();
+
+        /// <summary>
+        /// Asynchronously retrieve the raw binary product of compilation. The file data streamed will be of the format
+        /// specified in the session parameters.  If the compilation result was not successful, this will throw a
+        /// FIleNotFoundException.  
+        /// </summary>
+        /// <returns>A stream with the file contents</returns>
+        Task<Stream> GetProduct();
+    }
+
+    /// <summary>
+    /// A handle to the results of a compilation. Can be used to access the result state, the log data,
+    /// and the resulting product if there is one.
+    /// </summary>
+    internal class CompileResult : ICompileResult
     {
         private readonly FlurlClient _client;
         private readonly SessionInfo _info;
@@ -18,8 +46,17 @@ namespace LatexClient
             _info = info;
         }
 
+        /// <summary>
+        /// Gets whether or not the compilation completed successfully. If the compilation was not successful
+        /// there will be no product.
+        /// </summary>
         public bool IsSuccessful => _info.Status == "success";
 
+        /// <summary>
+        /// Asynchronously retrieve the compilation log, which can be used to debug errors in the case that the
+        /// compilation failed.
+        /// </summary>
+        /// <returns>A string containing the entire compiler log</returns>
         public async Task<string> GetLog()
         {
             var result = await _client.Request(_info.Log.Href)
@@ -28,8 +65,18 @@ namespace LatexClient
             return Encoding.UTF8.GetString(result);
         }
 
+        /// <summary>
+        /// Asynchronously retrieve the raw binary product of compilation. The file data streamed will be of the format
+        /// specified in the session parameters.  If the compilation result was not successful, this will throw a
+        /// FIleNotFoundException.  
+        /// </summary>
+        /// <returns>A stream with the file contents</returns>
         public async Task<Stream> GetProduct()
         {
+            if (!IsSuccessful) 
+                throw new FileNotFoundException(
+                    "The compilation was not successful, so there is not product file to retrieve. Check the IsSuccessful flag before attempting to get the product");
+
             var result = await _client.Request(_info.Product.Href)
                 .GetStreamAsync();
             return result;
--- LatexClient/LatexSession.cs
+++ LatexClient/LatexSession.cs
@@ -1,18 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Dynamic;
-using System.IO;
-using System.Net;
-using System.Threading.Tasks;
-using Flurl;
+﻿using Flurl;
 using Flurl.Http;
 using Flurl.Http.Configuration;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace LatexClient
 {
-    public class LatexSession
+    public interface ILatexSession
+    {
+        /// <summary>
+        /// Get the full URI of the session resource on the server
+        /// </summary>
+        Uri SessionUri { get; }
+
+        /// <summary>
+        /// Asynchronously get the session information from the server
+        /// </summary>
+        /// <returns></returns>
+        Task<SessionInfo> GetInfo();
+
+        /// <summary>
+        /// Upload a file to the session from a stream. The filename provided is the filename to save the data to
+        /// within the session, and can include subdirectory path elements if necessary. The server will not preserve
+        /// paths which attempt to leave the session folder.
+        /// </summary>
+        /// <param name="fileName">The filename and relative path to save the file as within the session</param>
+        /// <param name="stream">A stream containing the file data</param>
+        /// <returns></returns>
+        Task UploadFile(string fileName, Stream stream);
+
+        /// <summary>
+        /// Upload a file to the session from a local file. The filename provided is the filename to save the data to
+        /// within the session, and can include sub-directory path elements if necessary. The server will not preserve
+        /// paths which attempt to leave the session folder.  The file path provided is the path to the file locally,
+        /// which will be opened in read-only mode and its contents sent to the server.
+        /// </summary>
+        /// <param name="fileName">The filename and relative path to save the file as within the session</param>
+        /// <param name="filePath">A path to the file locally which will be opened and saved to the server</param>
+        /// <returns></returns>
+        Task UploadFile(string fileName, string filePath);
+
+        /// <summary>
+        /// Send the command to the server to begin compilation of the session, then asynchronously wait for its results.
+        /// </summary>
+        /// <returns>The result of the compilation, successful or not</returns>
+        Task<ICompileResult> Compile(CancellationToken token = default);
+    }
+
+    /// <summary>
+    /// The LatexSession class is a handle to a compilation session resource created on the server and provides all
+    /// of the methods for interacting with that session.  
+    /// </summary>
+    internal class LatexSession : ILatexSession
     {
         private readonly FlurlClient _client;
         private readonly string _sessionPath;
@@ -37,15 +80,31 @@ namespace LatexClient
             });
         }
 
+        /// <summary>
+        /// Get the full URI of the session resource on the server
+        /// </summary>
         public Uri SessionUri => _client.BaseUrl.AppendPathSegment(_sessionPath).ToUri();
 
-        public async Task GetInfo()
+        /// <summary>
+        /// Asynchronously get the session information from the server
+        /// </summary>
+        /// <returns></returns>
+        public async Task<SessionInfo> GetInfo()
         {
             _info = await _client.Request(_sessionPath)
                 .GetAsync()
                 .ReceiveJson<SessionInfo>();
+            return _info;
         }
 
+        /// <summary>
+        /// Upload a file to the session from a stream. The filename provided is the filename to save the data to
+        /// within the session, and can include subdirectory path elements if necessary. The server will not preserve
+        /// paths which attempt to leave the session folder.
+        /// </summary>
+        /// <param name="fileName">The filename and relative path to save the file as within the session</param>
+        /// <param name="stream">A stream containing the file data</param>
+        /// <returns></returns>
         public async Task UploadFile(string fileName, Stream stream)
         {
             if (_info is null)
@@ -55,10 +114,28 @@ namespace LatexClient
                 .PostMultipartAsync(p => p.AddFile(fileName, stream, fileName));
         }
 
-        public async Task<CompileResult> Compile()
+        /// <summary>
+        /// Upload a file to the session from a local file. The filename provided is the filename to save the data to
+        /// within the session, and can include sub-directory path elements if necessary. The server will not preserve
+        /// paths which attempt to leave the session folder.  The file path provided is the path to the file locally,
+        /// which will be opened in read-only mode and its contents sent to the server.
+        /// </summary>
+        /// <param name="fileName">The filename and relative path to save the file as within the session</param>
+        /// <param name="filePath">A path to the file locally which will be opened and saved to the server</param>
+        /// <returns></returns>
+        public Task UploadFile(string fileName, string filePath)
+        {
+            return UploadFile(fileName, File.OpenRead(filePath));
+        }
+
+        /// <summary>
+        /// Send the command to the server to begin compilation of the session, then asynchronously wait for its results.
+        /// </summary>
+        /// <returns>The result of the compilation, successful or not</returns>
+        public async Task<ICompileResult> Compile(CancellationToken token = default)
         {
             var response = await _client.Request(SessionUri)
-                .PostJsonAsync(new {finalize = true});
+                .PostJsonAsync(new {finalize = true}, cancellationToken: token);
 
             while (true)
             {
@@ -68,10 +145,10 @@ namespace LatexClient
                     return new CompileResult(_client, _info);
                 }
 
-                await Task.Delay(TimeSpan.FromSeconds(1.0));
+                token.ThrowIfCancellationRequested();
+
+                await Task.Delay(TimeSpan.FromSeconds(1.0), token);
             }
-
-
         }
     }
 }
